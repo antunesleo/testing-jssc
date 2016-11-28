f3ba5220cad69457716d7e0764ac3e4416443180
vini <vini@VINI-PC>
2016-11-28 13:52:29 -0200

    loop for read message

--- src/br/com/devcoffee/test/TestingJssc.java
+++ src/br/com/devcoffee/test/TestingJssc.java
@@ -10,10 +10,10 @@ public class TestingJssc {
 	
 	public static void main(String []args) {
 		//Serial port configuration variables
-		String comPort = null;
-		int baudeRate = 0;
-		int dataBits = 0;
-		int stopBits = 0;
+		String comPort = "/dev/pts/0";
+		int baudeRate = 9600;
+		int dataBits = 8;
+		int stopBits = 1;
 		int parity = 0;
 		
 		Scanner scanner = new Scanner(System.in); //Scanner to get info from the user
@@ -26,7 +26,7 @@ public class TestingJssc {
 		}
 		
 		System.out.println(System.getProperty("line.separator")); //Create a separator for the current OS
-		
+/*		
 		//Getting Serial port configuration
 		System.out.println("Type com port: ");
 		comPort = scanner.nextLine(); 
@@ -37,14 +37,21 @@ public class TestingJssc {
 		System.out.println("Type stop bits: ");
 		stopBits = Integer.parseInt(scanner.nextLine());
 		System.out.println("Type parity: ");
-		parity = Integer.parseInt(scanner.nextLine());
+		parity = Integer.parseInt(scanner.nextLine());*/
 		//Trying to build connection
 		SerialPort serialPort = new SerialPort(comPort);
 		try {
 			serialPort.openPort();//open serial port
 			System.out.println("Connection successfully");
 			serialPort.setParams(baudeRate, dataBits, stopBits, parity);
-			byte[] buffer = serialPort.readBytes(10);
+			String entrada = "";
+			while (!entrada.equals("exit")){
+				entrada = serialPort.readString();
+				if (entrada != null)
+					System.out.println(entrada);
+				else
+					entrada = "";
+			}
 			serialPort.closePort();
 		} catch (SerialPortException e) {
 			System.out.println("Connection failure, follow the errors below: ");
