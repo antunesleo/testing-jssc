a61d963fd1195191282e8bcb19de60093f57cb70
leo <leo@antunes>
2016-11-28 10:54:32 -0200

    Crated setup for serial information in running time

--- src/br/com/devcoffee/test/TestingJssc.java
+++ src/br/com/devcoffee/test/TestingJssc.java
@@ -1,31 +1,53 @@
 package br.com.devcoffee.test;
 
+import java.util.Scanner;
+
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
 
 public class TestingJssc {
-	//Info for the Serial Port
-	public static final int BAUDE_RATE = 9600;
-	public static final int DATA_BITS = 8;
-	public static final int STOP_BITS = 1;
-	public static final int PARITY = 0;
-		
+	
 	public static void main(String []args) {
-		//Getting all serial port names
+		//Serial port configuration variables
+		String comPort = null;
+		int baudeRate = 0;
+		int dataBits = 0;
+		int stopBits = 0;
+		int parity = 0;
+		
+		Scanner scanner = new Scanner(System.in); //Scanner to get info from the user
+		
+		//Getting all serial port names available on device
+		System.out.println("Listening all com ports available on device");
 		String[] portNames = SerialPortList.getPortNames();
 		for (int i = 0; i > portNames.length; i++) {
 			System.out.println(portNames[i]);
 		}
 		
-		//Reading data from serial port 
-		SerialPort serialPort = new SerialPort("COM1");
+		System.out.println(System.getProperty("line.separator")); //Create a separator for the current OS
+		
+		//Getting Serial port configuration
+		System.out.println("Type com port: ");
+		comPort = scanner.nextLine(); 
+		System.out.println("Type baude rate: ");
+		baudeRate = Integer.parseInt(scanner.nextLine());
+		System.out.println("Type data bits: ");
+		dataBits = Integer.parseInt(scanner.nextLine());
+		System.out.println("Type stop bits: ");
+		stopBits = Integer.parseInt(scanner.nextLine());
+		System.out.println("Type parity: ");
+		parity = Integer.parseInt(scanner.nextLine());
+		//Trying to build connection
+		SerialPort serialPort = new SerialPort(comPort);
 		try {
 			serialPort.openPort();//open serial port
-			serialPort.setParams(BAUDE_RATE, DATA_BITS, STOP_BITS, PARITY);
+			System.out.println("Connection successfully");
+			serialPort.setParams(baudeRate, dataBits, stopBits, parity);
 			byte[] buffer = serialPort.readBytes(10);
 			serialPort.closePort();
 		} catch (SerialPortException e) {
+			System.out.println("Connection failure, follow the errors below: ");
 			e.printStackTrace();
 		}	
 	}
